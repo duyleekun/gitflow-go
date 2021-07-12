7351d8e201949e3fadae8ea35949d4e07af107f0
duyle <duyleekun@gmail.com>
2021-07-12 13:34:48 +0700

    Print more friendly doc

--- shared/logger.go
+++ shared/logger.go
@@ -3,11 +3,13 @@ package shared
 import (
 	"fmt"
 	"log"
+	"os"
 )
 
 func HandleError(err error, format string, args ...interface{}) {
 	if err != nil {
-		log.Fatalf("%s | error: %v", fmt.Sprintf(format, args...), err)
+		log.Output(2, fmt.Sprintf("%s | error: %v", fmt.Sprintf(format, args...), err))
+		os.Exit(1)
 	}
 	log.Output(2, fmt.Sprintf("%s | success", fmt.Sprintf(format, args...)))
 }
